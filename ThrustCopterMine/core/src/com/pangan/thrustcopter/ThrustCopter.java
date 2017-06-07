9e4e24fb8f4bf54cd691e4479396bd080c0e507e
Ernesto Gonzalez <ernesgonzalez.33.egc@icloud.com>
2017-06-07 17:30:52 +0200

    Añadido el fondo

--- ThrustCopterMine/core/src/com/pangan/thrustcopter/ThrustCopter.java
+++ ThrustCopterMine/core/src/com/pangan/thrustcopter/ThrustCopter.java
@@ -2,29 +2,44 @@ package com.pangan.thrustcopter;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ThrustCopter extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	FPSLogger fpsLogger;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		fpsLogger = new FPSLogger();
+		camera = new OrthographicCamera();
+		background = new Texture("background.png");
 	}
 
 	@Override
 	public void render () {
+
+		//Limpieza de la pantalla
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		//FPS
 		fpsLogger.log();
+
+		//Añado la cámara
+		camera.setToOrtho(false, 800, 480);
+
+		//Actualizo escena
 		updateScene();
+
+		//Dibujo la escena
 		drawScene();
 	}
 
@@ -33,6 +48,15 @@ public class ThrustCopter extends ApplicationAdapter {
 	}
 
 	public void drawScene(){
+
+		//Preparo la cámara
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+		//Dibujo
+		batch.begin();
+		batch.draw(background, 0, 0);
+		batch.end();
 
 	}
 }
